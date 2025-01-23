29260231f9f39ccb3921f5b68b5c40071d652cd4
Ny0ttt <ameerul.bess@gmail.com>
2025-01-23 08:46:38 +0800

    update server lint and test configure react test and lint later

---
+++ react/eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"], 
+  
+  languageOptions: {sourceType: "module"}, 
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  },
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  
+];
